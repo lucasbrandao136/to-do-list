54f52da01a245dc77625503ac87e766dcf98f27d
Lucas Brandão <lucas.silva.brandao10@gmail.com>
2025-04-19 18:47:29 -0300

    feat: criando init.sql inicial

---
+++ sql/init.sql
@@ -0,0 +1,27 @@
+-- Criação das tabelas
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  registered_at TIMESTAMP NOT NULL,
+  enabled_at BOOLEAN,
+  birthday DATE NOT NULL,
+  full_name VARCHAR(255) NOT NULL,
+  display_name VARCHAR(255) NOT NULL,
+  user_name VARCHAR(255) NOT NULL,
+  photo_url VARCHAR(255)
+);
+
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  photo_url VARCHAR(255),
+  completed BOOLEAN DEFAULT false,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP,
+  due_date TIMESTAMP
+);
+
