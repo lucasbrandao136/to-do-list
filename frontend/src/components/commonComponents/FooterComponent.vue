7fd67a1e1f992235da41144f5bfe92f9e1eb24f9
Lucas Brandão <lucas.silva.brandao10@gmail.com>
2025-05-08 18:25:57 -0300

    chore: editando nome do app no footer

--- frontend/src/components/commonComponents/FooterComponent.vue
+++ frontend/src/components/commonComponents/FooterComponent.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="bg-white shadow-inner text-center py-3 text-sm text-gray-500">
-    © 2025 Meu App - Todos os direitos reservados
+    © 2025 TaskFlow - Todos os direitos reservados
   </footer>
 </template>
